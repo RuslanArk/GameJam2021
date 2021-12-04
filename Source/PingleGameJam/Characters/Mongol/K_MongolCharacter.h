d40a3e6bef8e56033c0ac36928113f7536cdc35e
Ruslan Kornis <RKornis@users.noreply.github.com>
2021-12-04 12:58:25 +0200

    Random characters spawn

---
+++ Source/PingleGameJam/Characters/Mongol/K_MongolCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PingleGameJam/Player/K_BaseCharacter.h"
+#include "K_MongolCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGLEGAMEJAM_API AK_MongolCharacter : public AK_BaseCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PingleGameJam/Characters/Yaga/K_YagaCharacter.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PingleGameJam/Characters/Yaga/K_YagaCharacter.h"
+
